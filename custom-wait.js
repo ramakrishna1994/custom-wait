3dc9efdebce183caba529e201e990ee844bf3ad5
RAMAKRISHNA SARADHI <ramakrishnasaradhi@gmail.com>
2016-05-27 18:22:50 +0530

    Added Custom wait js file version 1.0.0

---
+++ custom-wait.js
@@ -0,0 +1,14 @@
+
+
+module.exports = {
+  
+	customWait : function(milliSeconds)
+   {
+          var i;
+         for(i=0;i<=milliSeconds*2;i++)
+         {
+           //console.log("loop is executing in function");
+         }
+         return true;
+   }
+}
